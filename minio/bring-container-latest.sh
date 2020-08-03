325c68f31b7bb2728b8d24b7e561bc3bcd5b98d7
Weynand Kuijpers <weynandkuijpers@gmail.com>
2020-08-03 11:27:01 +0200

    Update bring-container-latest.sh

--- minio/bring-container-latest.sh
+++ minio/bring-container-latest.sh
@@ -4,3 +4,9 @@ apt update
 apt upgrade -y
 apt install git -y
 apt install bmon -y
+mkdir $HOME/opt
+cd $HOME/opt
+git clone https://github.com/weynandkuijpers/mytrunk.git
+cd mytrunk/minio
+wget https://dl.min.io/client/mc/release/linux-amd64/mc
+chmod +x mc
